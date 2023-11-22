a95b5bc115c045b8caaf54e8f83c833b1fb2b14d
Diego Giraldo <diegom.giraldoga@gmail.com>
2023-11-22 16:05:55 -0500

    Proyecto dia 2

--- dia 2 (Programa un Calculador de Nombres)/proyecto_dia_2.py
+++ dia 2 (Programa un Calculador de Nombres)/proyecto_dia_2.py
@@ -1,12 +1,15 @@
-# curso = "Python"
+'''
+La situación es esta: tú trabajas en una empresa donde los vendedores reciben comisiones
+del 13% por sus ventas totales, y tu jefe quiere que ayudes a los vendedores a calcular sus
+comisiones creando un programa que les pregunte su nombre y cuánto han vendido en este
+mes. Tu programa le va a responder con una frase que incluya su nombre y el monto que le
+corresponde por las comisiones. 
+'''
 
-# print('estás tomando un curso de %s'.capitalize()%(curso ))
+nombre_vendedor = input('Ingrese nombre del vendedor: ').title()
+ventas = float(input('Ventas totales del mes: '))
+comision = round((ventas*0.13))
 
-# -----------------------------------------------------------
-
-# num1 = 7.5
-# num2 = 2.5
-
-# print(num1+num2)
-
-print('/a')
+print(f'''El vendedor := {nombre_vendedor.upper()}
+Comision mes:= ${comision}''')
+            
