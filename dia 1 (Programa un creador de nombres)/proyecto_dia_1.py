96a6fe03c2d3237f6fa17618809f432f2ad2040e
Diego Giraldo <diegom.giraldoga@gmail.com>
2023-11-22 14:35:58 -0500

    segundo dia course python

---
+++ dia 1 (Programa un creador de nombres)/proyecto_dia_1.py
@@ -0,0 +1,8 @@
+'''
+Cree un programa que pueda asignar el nombre del producto 
+'''
+
+favorite_color = input('Ingrese el nombre de su color favorito: ')
+favorite_animal = input('Ingrese el nombre de su animal favorito: ')
+
+print("la cerveza se llama\n\"%s %s\"".capitalize()%(favorite_animal, favorite_color))
---
+++ dia 2 (Programa un Calculador de Nombres)/proyecto_dia_2.py
@@ -0,0 +1,12 @@
+# curso = "Python"
+
+# print('estás tomando un curso de %s'.capitalize()%(curso ))
+
+# -----------------------------------------------------------
+
+# num1 = 7.5
+# num2 = 2.5
+
+# print(num1+num2)
+
+print('/a')
